1847e703f8fa740529a281c24ca70328b6b76612
DJGump <dgump31@outlook.com>
2020-07-29 17:03:04 -0400

    Started sql exam

---
+++ dgump-sql-exam.sql
@@ -0,0 +1,54 @@
+-- 1
+SELECT 
+   table_name, 
+   column_name, 
+   data_type 
+FROM 
+   information_schema.columns
+WHERE 
+   table_name = 'naep';
+   
+-- 2
+SELECT *
+FROM NAEP 
+LIMIT 50;
+
+--3
+SELECT state
+	   ,COUNT(*)
+	   ,AVG(avg_math_4_score) AS avg_avg_math4
+	   ,MIN(avg_math_4_score) AS MIN_avg_math4
+	   ,MAX(avg_math_4_score) AS max_avg_math4
+FROM naep
+GROUP BY state
+ORDER BY state ASC;
+
+-- 4
+SELECT state
+	   ,COUNT(*)
+	   ,AVG(avg_math_4_score) AS avg_avg_math4
+	   ,MIN(avg_math_4_score) AS MIN_avg_math4
+	   ,MAX(avg_math_4_score) AS max_avg_math4
+FROM naep
+GROUP BY state
+HAVING MAX(avg_math_4_score) - MIN(avg_math_4_score) > 30
+ORDER BY state ASC;
+
+-- 5
+SELECT state AS bottom_10_states
+	   ,avg_math_4_score
+FROM naep
+WHERE year = 2000
+ORDER BY avg_math_4_score ASC
+LIMIT 10;
+
+-- 6
+SELECT ROUND(AVG(avg_math_4_score), 2) AS avg_math4_2000
+FROM naep
+WHERE year = 2000;
+
+-- 7
+
+
+
+
